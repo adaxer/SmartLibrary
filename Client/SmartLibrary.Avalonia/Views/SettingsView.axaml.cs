bdd30fd071e62215cffa9e0b4e5e5614934da54b
Andreas Daxer <info@andreas-daxer.de>
2024-09-14 19:56:11 +0200

    Prepare Resources

---
+++ Client/SmartLibrary.Avalonia/Views/SettingsView.axaml.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Controls;
+
+namespace SmartLibrary.Avalonia.Views;
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
