74325af0d80312de500fd712f1f27b0839aa4aad
Andreas Daxer <info@andreas-daxer.de>
2024-09-18 17:36:45 +0200

    Nuget Updates, Styles

---
+++ Client/SmartLibrary.Avalonia/Views/DesignData.cs
@@ -0,0 +1,13 @@
+﻿using NSubstitute;
+using SmartLibrary.Common.Interfaces;
+
+namespace SmartLibrary.Common.ViewModels;
+
+public static class DesignData
+{
+    public static LoginViewModel DesignLoginViewModel => new LoginViewModel(Substitute.For<IUserClient>())
+    {
+        UserName = "DesignUser",
+        Email = "User@Design.com"
+    };
+}
