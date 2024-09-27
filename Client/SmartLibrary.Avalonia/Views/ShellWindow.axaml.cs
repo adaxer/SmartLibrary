e2a2e51583f23345b3a4f15c1d1c7f8248b0f7ad
Andreas Daxer <info@andreas-daxer.de>
2024-09-27 23:11:00 +0200

    Animation per Code

--- Client/SmartLibrary.Avalonia/Views/ShellWindow.axaml.cs
+++ Client/SmartLibrary.Avalonia/Views/ShellWindow.axaml.cs
@@ -1,4 +1,12 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.Animation.Easings;
+using Avalonia.Animation;
+using Avalonia;
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Rendering.Composition.Animations;
+using Avalonia.Styling;
+using System.Threading.Tasks;
 
 namespace SmartLibrary.Avalonia.Views;
 public partial class ShellWindow : Window
@@ -6,5 +14,40 @@ public partial class ShellWindow : Window
     public ShellWindow()
     {
         InitializeComponent();
+    }
+
+    protected override async void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        var marginAnimation = new Animation
+        {
+            Duration = TimeSpan.FromMilliseconds(1000),
+            Easing = new CubicEaseOut(),
+            Children =
+            {
+                new KeyFrame
+                {
+                    Setters = { 
+                        new Setter(StackPanel.MarginProperty, new Thickness(0, 0, -300, 0)), 
+                        new Setter(StackPanel.OpacityProperty, 0.0) 
+                    },
+                    Cue = new Cue(0d)
+                },
+                new KeyFrame
+                {
+                    Setters = { 
+                        new Setter(StackPanel.MarginProperty, new Thickness(0, 0, 0, 0)),
+                        new Setter(StackPanel.OpacityProperty, 1.0)
+                    },
+                    Cue = new Cue(1d) 
+                }
+            }
+        };
+
+        await Task.Delay(4000);
+        await marginAnimation.RunAsync(_internetCheck);
+
+        _internetCheck.Margin = new();
     }
 }
