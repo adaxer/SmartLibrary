acdeaf72e32e45ffc1e20a72421880e09c4d8fd7
Andreas Daxer <info@andreas-daxer.de>
2024-09-27 11:41:55 +0200

    LoadMoreBehavior ready

---
+++ Client/SmartLibrary.Avalonia/Behaviors/LoadMoreBehavior.cs
@@ -0,0 +1,72 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using Avalonia.Xaml.Interactivity;
+using System.Windows.Input;
+
+namespace SmartLibrary.Avalonia.Behaviors;
+
+public class LoadMoreBehavior : Behavior<ItemsControl>
+{
+    public static readonly StyledProperty<ICommand> LoadMoreCommandProperty =
+         AvaloniaProperty.Register<LoadMoreBehavior, ICommand>(nameof(LoadMoreCommand));
+    private ScrollViewer? _scrollViewer;
+
+    public ICommand LoadMoreCommand
+    {
+        get => GetValue(LoadMoreCommandProperty);
+        set => SetValue(LoadMoreCommandProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        // Sobald dieses Behavior auf einem ItemsControl gesetzt wird,
+        // wird OnAttached von Avalonia gerufen und der OnloadedEvent abgewartet
+
+        base.OnAttached();
+
+        if (AssociatedObject != null)
+        {
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+    }
+
+    private void AssociatedObject_Loaded(object? sender, RoutedEventArgs e)
+    {
+        //  ScrollChangedEvent (eines enthaltenen ScrollViewers) fangen 
+        _scrollViewer = AssociatedObject.FindDescendantOfType<ScrollViewer>();
+        if (_scrollViewer == null)
+        {
+            return; // Kein ScrollViewer - kein LoadMore 
+        }
+
+        _scrollViewer.ScrollChanged += OnScrollChanged;
+        AssociatedObject!.Loaded -= AssociatedObject_Loaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+
+        if (_scrollViewer != null)
+        {
+            _scrollViewer.ScrollChanged -= OnScrollChanged;
+        }
+    }
+
+    private void OnScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        if(_scrollViewer==null)
+        {
+            return;
+        }
+        if (_scrollViewer.Offset.Y >= _scrollViewer.ScrollBarMaximum.Y && _scrollViewer.Offset.Y>0)
+        {
+            if (LoadMoreCommand?.CanExecute(null) == true)
+            {
+                LoadMoreCommand.Execute(null);
+            }
+        }
+    }
+}
